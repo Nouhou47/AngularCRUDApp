ca11f5fec68b49f327dbc199bf5a91edd6a864fb
JellyBean <nouhoundach@gmail.com>
2020-12-31 11:45:20 +0100

    First Commit, BookService already created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { TutorialDatailsComponent } from './components/tutorial-datails/tutorial-datails.component';
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTutorialComponent,
+    TutorialDatailsComponent,
+    TutorialsListComponent
   ],
   imports: [
     BrowserModule,
