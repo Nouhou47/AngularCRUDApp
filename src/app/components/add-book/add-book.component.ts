881581285be49d9074e96b44977c5355c4bc9828
JellyBean <nouhoundach@gmail.com>
2020-12-31 20:08:25 +0100

    add-book.component.html added to the app

--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-add-book',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBookComponent implements OnInit {
 
-  constructor() { }
+  book = {
+    name: '',
+    description: '',
+    sold: false
+  };
+
+  submitted = false;
+
+  constructor(private bookService: BookService) { }
 
   ngOnInit(): void {
+  }
+
+  saveBook(): void {
+    const data = {
+      name: this.book.name,
+      description: this.book.description
+    };
+
+    this.bookService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+        }
+      );
+  }
+
+  newBook(): void {
+    this.submitted = false;
+    this.book = {
+      name: '',
+      description: '',
+      sold: false
+    };
+
   }
 
 }
